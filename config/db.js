de64b08e5bd76f0622558a63460205aa1c511eee
Deepak Jain <deepakrjain7@gmail.com>
2025-07-16 18:47:11 +0530

    deployment ready

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/guestOnboarding");
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log('MongoDB connected successfully!');
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
